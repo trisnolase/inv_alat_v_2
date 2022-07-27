e078a428a5d14e2584d57ef0daec402f466df22d
trisnolase <trisnolase@gmail.com>
2022-07-27 15:01:43 +0700

    fix pencarian kosong

--- view_data/pencarian.php
+++ view_data/pencarian.php
@@ -1,5 +1,4 @@
 <?php
-//function untuk meng-highlight atau menandai hasil pencarian
 function highlight($text, $keyword)
 {
     $keyword = explode(" ", $keyword);
@@ -10,24 +9,27 @@ function highlight($text, $keyword)
     }
     return $text;
 }
-// keyword kata-kata pencarian
-// $keyword = 'tempat';
 $keyword = $_POST['cari'];
-
 $sql = mysqli_query($dblink, "SELECT * from alat_view where nama_peralatan like '%$keyword%'");
 ?>
 <div class='card'>
     <div class='card-header card-header-primary'>
         <h4 class='card-title'>Pencarian</h4>
-        <p class='card-category'>Hasil pencarian dengan kata kunci <strong><span class='text-light'>" <?php echo $keyword ?> "</span></strong></p>
+        <p class='card-category'>Hasil pencarian dengan kata kunci <strong><span class='text-light'> <?php echo '"' . $keyword . '"' ?> </span></strong></p>
     </div>
     <div class='card-body'>
-        <ol>
-            <?php
-            while ($gr = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
-                $xidbk = isset($gr['nama_peralatan']) ? $gr['nama_peralatan'] : '';
-                echo "<li><div class='p-2'>" . highlight($xidbk, $keyword) . "</div></li>";
-            } ?>
-        </ol>
+
+        <?php
+        if ($keyword == "") {
+            echo "tidak ada hasil";
+        } else { ?>
+            <ol>
+                <?php
+                while ($gr = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+                    $xidbk = isset($gr['nama_peralatan']) ? $gr['nama_peralatan'] : '';
+                    echo "<li><div class='p-2'>" . highlight($xidbk, $keyword) . "</div></li>";
+                } ?>
+            </ol>
+        <?php } ?>
     </div>
 </div>
