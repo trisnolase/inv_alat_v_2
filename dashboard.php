e698e0dfeea5761338244e1af5250542541a6849
trisnolase <trisnolase@gmail.com>
2022-09-06 17:28:57 +0700

    fa update

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,6 @@ $hal = $_GET['apage'];
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" href="assets/g-font.css">
-  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
   <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
 
